edbddf830eaed0c484679e8a6d935ee86d64b593
lizstangle <lizstangle@me.com>
2021-02-09 18:02:30 -0800

    getting started

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """Shows a greeting to the user."""
+    return 'Are you there, world? It\'s me, Ducky!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
